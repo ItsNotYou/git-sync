7d28be142a312f73404b99b5fdcd179eda124f85
Hendrik Geßner <hgessner@uni-potsdam.de>
2020-01-13 14:08:51 +0100

    Added error report sending via email.

--- git_sync.py
+++ git_sync.py
@@ -1,9 +1,14 @@
 import os
 import shutil
+import smtplib
 import stat
 import subprocess
 import sys
 import tempfile
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 import yaml
 
 
@@ -35,18 +40,39 @@ def run_command(args, repo_dir, log):
         raise IOError("Command failed")
 
 
-def report_error(repo, repo_dir, log):
+def report_error(repo, repo_dir, log, report_cfg):
     print(f"Manual intervention required for {repo} in {repo_dir}, log below")
-    print()
 
-    log.flush()
-    log.seek(0)
-
-    print(log.read())
-    print()
+    subject = f"Error during git-sync for {repo}"
+    body = f"Manual intervention required for {repo}. See the attached log for details."
+    send_email(report_cfg, subject, body, log)
 
 
-def sync_repositories(work_dir, repo_name, git_url_1, git_url_2):
+def send_email(email_cfg, subject, body, log):
+    # compose email
+    msg = MIMEMultipart()
+    msg['From'] = email_cfg["from"]
+    msg['To'] = email_cfg["to"]
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+
+    # set attachment
+    p = MIMEBase('text', 'plain')
+    p.set_payload(log.read())
+    p.add_header('Content-Disposition', f"attachment; filename={os.path.basename(log.name)}")
+    msg.attach(p)
+
+    # create SMTP session
+    s = smtplib.SMTP(email_cfg["host"], email_cfg["port"])
+    try:
+        s.starttls()
+        s.login(email_cfg["user"], email_cfg["password"])
+        s.sendmail(email_cfg["from"], email_cfg["to"], msg.as_string())
+    finally:
+        s.quit()
+
+
+def sync_repositories(work_dir, repo_name, git_url_1, git_url_2, report_cfg):
     with tempfile.NamedTemporaryFile(mode="w+t", prefix="git-sync-log-", suffix=".txt", delete=False) as log:
         print(f"Syncing {repo_name}, using directory {work_dir}, logging in {log.name}")
 
@@ -59,17 +85,24 @@ def sync_repositories(work_dir, repo_name, git_url_1, git_url_2):
             run_command(["git", "push", "--progress", "other", "master"], work_dir, log)
         except IOError:
             # a command went wrong, most probably a failed merge
-            report_error(repo_name, work_dir, log)
+            log.flush()
+            log.seek(0)
+            report_error(repo_name, work_dir, log, report_cfg)
 
 
 if __name__ == "__main__":
-    with open("config.yml", "r") as ymlfile:
-        cfg = yaml.safe_load(ymlfile)
+    # load basic configuration
+    with open("config.yml", "r") as yml_file:
+        cfg = yaml.safe_load(yml_file)
+
+    # load error report credentials
+    with open(os.path.expanduser(cfg["email_credentials"]), "r") as credentials_file:
+        report_cfg = yaml.safe_load(credentials_file)
 
     for repo in cfg["repositories"]:
         with tempfile.TemporaryDirectory(prefix="git-sync-") as work_dir:
             try:
-                sync_repositories(work_dir, repo['name'], repo["repo1"], repo["repo2"])
+                sync_repositories(work_dir, repo['name'], repo["repo1"], repo["repo2"], report_cfg)
             except:
                 print("Unhandled error occurred while synchronizing", repo['name'], sys.exc_info()[0])
 
