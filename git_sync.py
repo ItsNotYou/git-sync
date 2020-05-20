84a02e388dff4dc1f6264445e95b06a4494250cc
Hendrik Geßner <HENGE01@gmail.com>
2020-05-20 19:57:14 +0200

    Added command line feedback as comments.

--- git_sync.py
+++ git_sync.py
@@ -10,6 +10,40 @@ from gitsync.reporting import send_email
 
 def parse_arguments():
     # prepare command line parser
+    # Christians Vorschläge:
+    #
+    # - usage: git_sync.py [--verbose] [--workdir WORKING_DIRECTORY] [--mail TO [--smtp CONFIG]] repository [repository ...]
+    #        git_sync.py [--help]
+    # Hier sieht man, dass --smtp nur bei --mail verwendet werden kann.
+    #
+    # Es wäre praktisch, für alle üblichen Parameter eine Kurzform zu haben.
+    #
+    # -v      Verbose mode.  Causes ssh to print debugging messages about its progress.  This is
+    #              helpful in debugging connection, authentication, and configuration problems.  Multiple
+    #              -v options increase the verbosity.  The maximum is 3.
+    #
+    # Bei --workdir meinst du mit 'data' einen relativen Pfad, oder?
+    # Hier wäre vielleicht besser, den Pfade nicht vom aktuellen Ort abhängig zu machen und dem Ordner einen klarer zuordenbaren Namen zu geben, z.B. '~/git_sync'.
+    # Als Kurzfassung kannst du hier z.B. -w nehmen.
+    #    -w --workdir
+    #
+    # Hier ist die Frage, ob du eine Logfile verwenden willst, an welche du hinten anhängst:
+    #     -l --log FILE
+    # Das sorgt dafür, dass man auch ohne mail die Ausgaben bekommt
+    #
+    # Ich würde statt --use_mail und --to einfach nur:
+    #     -m --mail TO               send error report to recipient TO using 'mail' command
+    # verwenden, du weißt dann ja, dass eine Email versendet werden soll und per default 'mail' annehmen.
+    # Zusätzlich würde ich den Parameter:
+    #     -s --smtp CONFIG                  send mail using 'smtp' instead of 'mail', requires --mail
+    # direkt ergänzend verwenden, um den default von 'mail' auf 'smtp' zu ändern.
+    # Du kannst sogar überlegen, ob du die "TO" email hier ebenfalls ablegst, dann spart man sich den --mail parameter.
+    #
+    # Der Parameter "repositories" ist nicht genau genug beschrieben,
+    # hier ist mir unklar, wie der Pfad auszusehen hat und warum "repositories" mit -es (Mehrzahl) in den Argumenten mehrfach vorkommt.
+    # Ich würde hier einfach
+    #     repository
+    # draus machen. (Ist Repository der richtige Begriff? Oder ist es eher der Pfad in einem Repository bzw. URL, die du angibst?)
     parser = argparse.ArgumentParser(description="Synchronize Git remotes repositories via pull and push.")
     parser.add_argument("--log", choices=["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"], default="WARNING",
                         help="set log output level, default is 'WARNING'")
